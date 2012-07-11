a64b2be06ff6c2ae542b95ac44bb937b8860ca2a
applecool <applecoolkmit@gmail.com>
2012-07-11 20:08:38 +0530

    Fixed the formatting

--- core/src/main/java/org/openmrs/contrib/metadatarepository/model/MetadataPackage.java
+++ core/src/main/java/org/openmrs/contrib/metadatarepository/model/MetadataPackage.java
@@ -42,7 +42,7 @@ public class MetadataPackage extends BaseObject {
 	private Long version;
 	private byte[] file;
 	private User user;
-	private Long downloadCount=0L;
+	private Long downloadCount = 0L;
 
 	/**
 	 * Default constructor - creates a new instance with no values set.
